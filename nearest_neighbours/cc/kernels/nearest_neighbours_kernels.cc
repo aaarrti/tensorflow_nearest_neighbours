b54c4d89d0932363f9b6198dbeac05805333a2d3
Artem Sereda <artem.sereda@campus.tu-berlin.de>
2022-12-04 09:17:47 +0100

    *

---
+++ nearest_neighbours/cc/kernels/nearest_neighbours_kernels.cc
@@ -0,0 +1,205 @@
+#include "tensorflow/core/framework/op_kernel.h"
+#include <cmath>
+#include "tensorflow/core/framework/tensor_types.h"
+#include "third_party/eigen3/unsupported/Eigen/CXX11/Tensor"
+#include "tensorflow/core/platform/types.h"
+#include "tensorflow/core/lib/core/threadpool.h"
+#include "third_party/eigen3/unsupported/Eigen/CXX11/Tensor"
+
+#define EIGEN_USE_THREADS
+
+
+std::vector<int32_t> findIndexOfNearestNeighbour(
+        const tensorflow::Tensor *token_embeddings,
+        const Eigen::Matrix<
+                float, Eigen::Dynamic,
+                Eigen::Dynamic,
+                Eigen::RowMajor
+        > &eigen_embedding_matrix,
+        int32_t batch_index,
+        int32_t vocab_size,
+        int32_t embedding_dim,
+        int32_t sequence_length
+) {
+    // Convert to Eigen::Matrix for computation
+    const auto sequence = Eigen::Map<const Eigen::Matrix<
+            float,           /* scalar element type */
+            Eigen::Dynamic,  /* num_rows is a run-time value */
+            Eigen::Dynamic,  /* num_cols is a run-time value */
+            Eigen::RowMajor  /* tensorflow::Tensor is always row-major */
+    >>(
+            token_embeddings->SubSlice(batch_index).flat<float>().data(),   /* ptr to data */
+            vocab_size,                                                     /* num_rows */
+            embedding_dim                                                   /* num_cols */
+    );
+
+
+    auto argmin_vector = std::vector<int32_t>(sequence_length);
+
+    for (auto token_index = 0; token_index != sequence_length; token_index++) {
+
+        auto distances = std::vector<float>(vocab_size);
+
+        for (auto matrix_row_index = 0; matrix_row_index != vocab_size; matrix_row_index++) {
+            // Compute distance between current embedding and each matrix row
+            const auto row = eigen_embedding_matrix.row(matrix_row_index);
+            const auto embedding = sequence.row(token_index);
+            const auto dist = static_cast<float>((row - embedding).squaredNorm());
+            distances[matrix_row_index] = dist;
+        }
+
+        // Find index of the smallest distance
+        auto it = std::min_element(std::begin(distances), std::end(distances));
+        auto argmin = static_cast<int32_t>(std::distance(std::begin(distances), it));
+        argmin_vector[token_index] = argmin;
+    }
+    return argmin_vector;
+}
+
+
+class NearestNeighboursOp : public tensorflow::OpKernel {
+public:
+
+    explicit NearestNeighboursOp(tensorflow::OpKernelConstruction *context) : tensorflow::OpKernel(context) {}
+
+    void Compute(tensorflow::OpKernelContext *context) override {
+
+        // Create inputs
+        const tensorflow::Tensor *token_embeddings = nullptr;
+        const tensorflow::Tensor *embedding_matrix = nullptr;
+        // Check inputs were passed
+        OP_REQUIRES_OK(context, context->input("token_embeddings", &token_embeddings));
+        OP_REQUIRES_OK(context, context->input("embedding_matrix", &embedding_matrix));
+
+        // Get input dims
+        const auto batch_size = static_cast<int32_t>(token_embeddings->dim_size(0));
+        const auto vocab_size = static_cast<int32_t>(embedding_matrix->dim_size(0));
+        const auto sequence_length = static_cast<int32_t>(token_embeddings->dim_size(1));
+        const auto embedding_dim = static_cast<int32_t>(token_embeddings->dim_size(2));
+        // Shape Input
+        auto embedding_matrix_shaped = embedding_matrix->shaped<float, 2>({vocab_size, embedding_dim});
+
+
+        // Create output
+        tensorflow::Tensor *output_tensor = nullptr;
+        OP_REQUIRES_OK(context, context->allocate_output(0, token_embeddings->shape(), &output_tensor));
+        // Reshape for indexing
+        const auto output_shaped = output_tensor->shaped<float, 3>({batch_size, sequence_length, embedding_dim});
+
+        // Convert to Eigen::Matrix for computation
+        const auto eigen_embedding_matrix = Eigen::Map<const Eigen::Matrix<
+                float,           /* scalar element type */
+                Eigen::Dynamic,  /* num_rows is a run-time value */
+                Eigen::Dynamic,  /* num_cols is a run-time value */
+                Eigen::RowMajor  /* tensorflow::Tensor is always row-major */
+        >>(
+                embedding_matrix->flat<float>().data(),     /* ptr to data */
+                vocab_size,                                 /* num_rows */
+                embedding_dim                               /* num_cols */
+        );
+
+        // Create thread pool for sharded execution
+        auto thread_pool = context->device()->tensorflow_cpu_worker_threads()->workers;
+
+        auto compute_shard = [
+                &sequence_length,
+                &token_embeddings,
+                &vocab_size,
+                &output_shaped,
+                &embedding_dim,
+                &eigen_embedding_matrix,
+                &embedding_matrix_shaped
+        ](int32_t start, int32_t stop) {
+
+            for (auto batch_index = start; batch_index != stop; batch_index++) {
+                auto argmin_vector = findIndexOfNearestNeighbour(token_embeddings, eigen_embedding_matrix,
+                                                                 batch_index, vocab_size, embedding_dim,
+                                                                 sequence_length);
+
+                for (auto token_index = 0; token_index != argmin_vector.size(); token_index++) {
+                    // Fill the output
+                    for (auto i = 0; i != embedding_dim; i++) {
+                        output_shaped({batch_index, token_index, i}) = embedding_matrix_shaped(
+                                {argmin_vector[token_index], i});
+                    }
+                }
+            }
+        };
+
+        // Run sharded Kernel
+        thread_pool->ParallelFor(batch_size, vocab_size * embedding_dim, std::move(compute_shard));
+    }
+};
+
+
+class NearestNeighboursIndexesOp : public tensorflow::OpKernel {
+public:
+
+    explicit NearestNeighboursIndexesOp(tensorflow::OpKernelConstruction *context) : tensorflow::OpKernel(
+            context) {}
+
+    void Compute(tensorflow::OpKernelContext *context) override {
+
+        // Create inputs
+        const tensorflow::Tensor *token_embeddings = nullptr;
+        const tensorflow::Tensor *embedding_matrix = nullptr;
+        // Check inputs were passed
+        OP_REQUIRES_OK(context, context->input("token_embeddings", &token_embeddings));
+        OP_REQUIRES_OK(context, context->input("embedding_matrix", &embedding_matrix));
+
+        // Get input dims
+        const auto batch_size = static_cast<int32_t>(token_embeddings->dim_size(0));
+        const auto vocab_size = static_cast<int32_t>(embedding_matrix->dim_size(0));
+        const auto sequence_length = static_cast<int32_t>(token_embeddings->dim_size(1));
+        const auto embedding_dim = static_cast<int32_t>(token_embeddings->dim_size(2));
+
+        // Create output
+        tensorflow::Tensor *output_tensor = nullptr;
+        OP_REQUIRES_OK(context, context->allocate_output(0, tensorflow::TensorShape({batch_size, sequence_length}),
+                                                         &output_tensor));
+        // Reshape for indexing
+        const auto output_shaped = output_tensor->shaped<int32_t, 2>({batch_size, sequence_length});
+
+        // Convert to Eigen::Matrix for computation
+        const auto eigen_embedding_matrix = Eigen::Map<const Eigen::Matrix<
+                float,           /* scalar element type */
+                Eigen::Dynamic,  /* num_rows is a run-time value */
+                Eigen::Dynamic,  /* num_cols is a run-time value */
+                Eigen::RowMajor  /* tensorflow::Tensor is always row-major */
+        >>(
+                embedding_matrix->flat<float>().data(),     /* ptr to data */
+                vocab_size,                                 /* num_rows */
+                embedding_dim                               /* num_cols */
+        );
+
+        // Create thread pool for sharded execution
+        auto thread_pool = context->device()->tensorflow_cpu_worker_threads()->workers;
+
+        auto compute_shard = [
+                &sequence_length,
+                &token_embeddings,
+                &vocab_size,
+                &output_shaped,
+                &embedding_dim,
+                &eigen_embedding_matrix
+        ](int32_t start, int32_t stop) {
+
+            for (auto batch_index = start; batch_index != stop; batch_index++) {
+                auto argmin_vector = findIndexOfNearestNeighbour(token_embeddings, eigen_embedding_matrix,
+                                                                 batch_index, vocab_size, embedding_dim,
+                                                                 sequence_length);
+
+                for (auto token_index = 0; token_index != argmin_vector.size(); token_index++) {
+                    output_shaped({batch_index, token_index}) = argmin_vector[token_index];
+                }
+            }
+        };
+
+        // Run sharded Kernel
+        thread_pool->ParallelFor(batch_size, vocab_size * embedding_dim, std::move(compute_shard));
+    }
+};
+
+
+REGISTER_KERNEL_BUILDER(Name("NearestNeighbours").Device(tensorflow::DEVICE_CPU), NearestNeighboursOp)
+REGISTER_KERNEL_BUILDER(Name("NearestNeighboursIndexes").Device(tensorflow::DEVICE_CPU), NearestNeighboursIndexesOp)
--- setup.py
+++ setup.py
@@ -1,36 +1,17 @@
-# Copyright 2018 The TensorFlow Authors. All Rights Reserved.
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#     http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-# ==============================================================================
-"""Setup for pip package."""
-from __future__ import absolute_import
-from __future__ import division
-from __future__ import print_function
-
-from setuptools import Extension
 from setuptools import find_packages
 from setuptools import setup
 from setuptools.dist import Distribution
-
+from setuptools.command.install import install
 
 __version__ = '0.0.1'
+
 REQUIRED_PACKAGES = [
-    'tensorflow >= 2.1.0',
+    # 'tensorflow-cpu == 2.10.0',
 ]
-project_name = 'tensorflow-custom-ops'
+
+project_name = 'nearest_neighbours'
 
 
-from setuptools.command.install import install
 class InstallPlatlib(install):
     def finalize_options(self):
         install.finalize_options(self)
@@ -38,20 +19,21 @@ class InstallPlatlib(install):
 
 
 class BinaryDistribution(Distribution):
-  """This class is needed in order to create OS specific wheels."""
+    """This class is needed in order to create OS specific wheels."""
 
-  def has_ext_modules(self):
-    return True
+    def has_ext_modules(self):
+        return True
 
-  def is_pure(self):
-    return False
+    def is_pure(self):
+        return False
+
 
 setup(
     name=project_name,
     version=__version__,
-    description=('tensorflow-custom-ops is an examples for custom ops for TensorFlow'),
-    author='Google Inc.',
-    author_email='opensource@google.com',
+    description="Nearest Neighbours for LM evaluation",
+    author='Artem Sereda',
+    author_email='artem.sereda@campus.tu-berlin.de',
     # Contained modules and scripts.
     packages=find_packages(),
     install_requires=REQUIRED_PACKAGES,
@@ -62,19 +44,11 @@ setup(
     cmdclass={'install': InstallPlatlib},
     # PyPI package information.
     classifiers=[
-        'Development Status :: 4 - Beta',
-        'Intended Audience :: Developers',
-        'Intended Audience :: Education',
         'Intended Audience :: Science/Research',
-        'License :: OSI Approved :: Apache Software License',
-        'Programming Language :: Python :: 2.7',
-        'Programming Language :: Python :: 3.4',
-        'Programming Language :: Python :: 3.5',
-        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.9',
         'Topic :: Scientific/Engineering :: Mathematics',
         'Topic :: Software Development :: Libraries :: Python Modules',
-        'Topic :: Software Development :: Libraries',
     ],
-    license='Apache 2.0',
     keywords='tensorflow custom op machine learning',
+    python_requires=">=3.7"
 )
