520282bd95cfb30e4f8b3f65ac8d73ff169179c4
aaarrti <artem@node47.ml.tu-berlin.de>
2022-12-18 00:02:06 +0100

    *

--- configure.sh
+++ configure.sh
@@ -14,9 +14,24 @@ function write_action_env_to_bazelrc() {
 # Remove .bazelrc if it already exist
 [ -e .bazelrc ] && rm .bazelrc
 
+
+# Check if we are building GPU or CPU ops, default CPU
+while [[ "$TF_NEED_CUDA" == "" ]]; do
+  read -p "Do you want to build ops again TensorFlow CPU pip package?"\
+" Y or enter for CPU (tensorflow-cpu), N for GPU (tensorflow). [Y/n] " INPUT
+  case $INPUT in
+    [Yy]* ) echo "Build with CPU pip package."; TF_NEED_CUDA=0;;
+    [Nn]* ) echo "Build with GPU pip package."; TF_NEED_CUDA=1;;
+    "" ) echo "Build with CPU pip package."; TF_NEED_CUDA=0;;
+    * ) echo "Invalid selection: " $INPUT;;
+  esac
+done
+
 # shellcheck disable=SC2207
 TF_CFLAGS=($(python -c 'import tensorflow as tf; print(" ".join(tf.sysconfig.get_compile_flags()))'))
 TF_LFLAGS="$(python -c 'import tensorflow as tf; print(" ".join(tf.sysconfig.get_link_flags()))')"
+
+write_to_bazelrc "build:cuda --define=using_cuda=true --define=using_cuda_nvcc=true"
 
 write_to_bazelrc "build --spawn_strategy=standalone"
 write_to_bazelrc "build --strategy=Genrule=standalone"
@@ -33,27 +48,6 @@ write_action_env_to_bazelrc "TF_HEADER_DIR" "${HEADER_DIR}"
 write_action_env_to_bazelrc "TF_SHARED_LIBRARY_DIR" "${SHARED_LIBRARY_DIR}"
 write_action_env_to_bazelrc "TF_SHARED_LIBRARY_NAME" ${SHARED_LIBRARY_NAME}
 
-# Check if we are building GPU or CPU ops, default CPU
-while [[ "$TF_NEED_CUDA" == "" ]]; do
-  read -p "Do you want to build ops again TensorFlow CPU pip package?" \
-    " Y or enter for CPU (tensorflow-cpu), N for GPU (tensorflow). [Y/n] " INPUT
-  case $INPUT in
-  [Yy]*)
-    echo "Build with CPU pip package."
-    TF_NEED_CUDA=0
-    ;;
-  [Nn]*)
-    echo "Build with GPU pip package."
-    TF_NEED_CUDA=1
-    ;;
-  "")
-    echo "Build with CPU pip package."
-    TF_NEED_CUDA=0
-    ;;
-  *) echo "Invalid selection: " $INPUT ;;
-  esac
-done
-
 write_action_env_to_bazelrc "TF_NEED_CUDA" ${TF_NEED_CUDA}
 
 if [[ "$TF_NEED_CUDA" == "1" ]]; then
@@ -69,7 +63,8 @@ if [[ "$TF_NEED_CUDA" == "1" ]]; then
     CUDA_VERSION=""
   fi
   write_action_env_to_bazelrc "TF_CUDA_VERSION" "${CUDA_VERSION}"
-  write_action_env_to_bazelrc "TF_CUDNN_VERSION" "7"
+  # TODO parse version from cat /usr/include/cudnn_version.h | grep CUDNN_MAJOR -A 2
+  write_action_env_to_bazelrc "TF_CUDNN_VERSION" "8"
 
   write_action_env_to_bazelrc "CUDNN_INSTALL_PATH" "/usr/lib/x86_64-linux-gnu"
   write_action_env_to_bazelrc "CUDA_TOOLKIT_PATH" "/usr/local/cuda"
