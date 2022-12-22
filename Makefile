c9cf102b0429dec2284f7a8e5d0da9e339326e8d
Artem Sereda <artem.sereda@campus.tu-berlin.de>
2022-12-22 23:04:49 +0100

    *

--- Makefile
+++ Makefile
@@ -1,38 +1,39 @@
-TF_CFLAGS=$(shell python3.9 -c 'import tensorflow as tf; print(" ".join(tf.sysconfig.get_compile_flags()))')
-TF_LFLAGS=$(shell python3.9 -c 'import tensorflow as tf; print(" ".join(tf.sysconfig.get_link_flags()))')
-
-
-CPU_SRC = cc/ops/nearest_neighbours_ops.cc cc/kernels/nearest_neighbours_kernels.cc
-CUDA_LIB = build/_nearest_neighbours_ops.cu.o
-
-C_FLAGS = $(TF_CFLAGS) -fPIC -std=c++17 -O3
-L_FLAGS = -shared $(TF_LFLAGS)
-TARGET_FLAG = -o build/_nearest_neighbours_ops.so
-
 test:
 	cp python/nearest_neighbours_test.py build;
-	cd build; python3.9 nearest_neighbours_test.py
+	cd build; python nearest_neighbours_test.py
 
 pip_pkg:
 	cp -f setup.py build
 	cp -f python/__init__.py build
 	cp -f python/nearest_neighbours.py build
-	cd build; python3 setup.py bdist_wheel
+	cd build; python setup.py bdist_wheel
+
+
+TF_CFLAGS=$(shell python -c 'import tensorflow as tf; print(" ".join(tf.sysconfig.get_compile_flags()))')
+TF_LFLAGS=$(shell python -c 'import tensorflow as tf; print(" ".join(tf.sysconfig.get_link_flags()))')
+
+
+CPU_SRC = cc/ops/nearest_neighbours_ops.cc cc/kernels/nearest_neighbours_kernels.cc
+CUDA_LIB = build/_nearest_neighbours_ops.cu.o
+
+C_FLAGS = ${TF_CFLAGS} -fPIC -std=c++17 -O3
+L_FLAGS = -shared ${TF_LFLAGS}
+TARGET_FLAG = -o build/_nearest_neighbours_ops.so
 
 cpu_kernel:
-	clang++ $(C_FLAGS) $(L_FLAGS) $(CPU_SRC) $(TARGET_FLAG)
+	g++ $(C_FLAGS) $(L_FLAGS) $(CPU_SRC) $(TARGET_FLAG)
 
 cuda_kernel:
-	nvcc -c $(TF_CFLAGS) \
+	nvcc -c $(TF_CFLAGS) $(L_FLAGS) \
 		-D CUDA=1 \
 		-x cu -Xcompiler -fPIC --expt-relaxed-constexpr \
 		cc/kernels/nearest_neighbours_kernels.cu \
 		-o $(CUDA_LIB)
-	clang++ $(CFLAGS) $(LDFLAGS) \
+	g++ $(C_FLAGS) $(L_FLAGS) \
 		-D CUDA=1  \
 		-I/usr/local/cuda/targets/x86_64-linux/include \
 		-L/usr/local/cuda/targets/x86_64-linux/lib -lcudart \
-		$(CPU_SRC) $(CUDA_LIB)
+		$(CPU_SRC) $(CUDA_LIB) $(TARGET_FLAG)
 
 
 metal_kernel:
--- cc/kernels/nearest_neighbours_kernels.cu
+++ cc/kernels/nearest_neighbours_kernels.cu
@@ -1,13 +1,13 @@
 #include "tensorflow/core/framework/op_kernel.h"
 #include "tensorflow/core/framework/tensor_types.h"
 #include "tensorflow/core/platform/types.h"
+#include "tensorflow/core/util/gpu_kernel_helper.h"
 #include "third_party/eigen3/unsupported/Eigen/CXX11/Tensor"
 #include "nearest_neighbours.h"
 
 
 #ifdef CUDA
 #define EIGEN_USE_GPU
-#endif
 
 
 namespace tensorflow {
@@ -18,27 +18,25 @@ namespace tensorflow {
     // Define the CUDA kernel.
     template<typename T>
     __global__ void NearestNeighboursCudaKernel(
-        const int32_t batch_size,
-        const int32_t vocab_size,
-        const int32_t sequence_length,
-        const int32_t embedding_dim,
-        const tensorflow::Tensor *token_embeddings,
-        const tensorflow::Tensor *embedding_matrix_shaped,
-        const Eigen::Matrix<T, Eigen::Dynamic, Eigen::Dynamic, Eigen::RowMajor> eigen_embedding_matrix,
-        tensorflow::Tensor *output_shaped
+        //const int32_t vocab_size,
+        //const int32_t embedding_dim,
+        //const tensorflow::Tensor *token_embeddings,
+        //const tensorflow::Tensor *embedding_matrix_shaped,
+        //const Eigen::Matrix<const T, Eigen::Dynamic, Eigen::Dynamic, Eigen::RowMajor> eigen_embedding_matrix,
+        //tensorflow::Tensor *output_shaped
     ) {
 
-      const auto sequence = Eigen::Map<
-          const Eigen::Matrix <T, Eigen::Dynamic, Eigen::Dynamic, Eigen::RowMajor>
-      >(token_embeddings->SubSlice(threadIdx.x).flat<T>().data(), vocab_size, embedding_dim);
-      auto distances = std::vector<T>(vocab_size);
-      const auto embedding = sequence.row(threadIdx.y);
+      //const auto sequence = Eigen::Map<
+      //    const Eigen::Matrix <const T, Eigen::Dynamic, Eigen::Dynamic, Eigen::RowMajor>
+      //>(token_embeddings->SubSlice(threadIdx.x).flat<T>().data(), vocab_size, embedding_dim);
+      //const auto distances = std::vector<T>(vocab_size);
+      //const auto embedding = sequence.row(threadIdx.y);
       // Find index of the smallest distance
-      auto argmin = nearest_neighbour_index<T>(vocab_size, embedding, eigen_embedding_matrix);
+      //const auto argmin = nearest_neighbour_index<T>(vocab_size, embedding, eigen_embedding_matrix);
       // Fill the output
-      for (auto i = 0; i != embedding_dim; i++) {
-        output_shaped({threadId.x, threadId.y, i}) = embedding_matrix_shaped({argmin, i});
-      }
+      //for (auto i = 0; i != embedding_dim; i++) {
+      //  output_shaped({threadIdx.x, threadIdx.y, i}) = embedding_matrix_shaped({argmin, i});
+      //}
     }
 
     template<typename T>
@@ -66,17 +64,8 @@ namespace tensorflow {
 
         auto output_shaped = output_tensor->shaped<T, 3>({batch_size, sequence_length, embedding_dim});
 
-        NearestNeighboursCudaKernel<float>
-        <<<block_count, thread_per_block, 0, device.stream()>>>(
-            batch_size,
-            vocab_size,
-            sequence_length,
-            embedding_dim,
-            token_embeddings,
-            embedding_matrix_shaped,
-            eigen_embedding_matrix,
-            output_shaped
-        );
+        //NearestNeighboursCudaKernel<float><<<block_count, thread_per_block, 0, device.stream()>>>(
+        //    vocab_size, embedding_dim, token_embeddings, embedding_matrix_shaped, eigen_embedding_matrix, output_shaped);
       }
     };
 
@@ -85,3 +74,5 @@ namespace tensorflow {
     struct NearestNeighboursFunctor<float, GPUDevice>;
   }
 }
+
+#endif
