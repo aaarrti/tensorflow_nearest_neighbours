c86db01ebd967b3eebe93d2a7de829daa47b957d
Artem Sereda <artem.sereda@campus.tu-berlin.de>
2022-12-29 13:38:06 +0100

    format code

--- nearest_neighbours/cc/ops/nearest_neighbours_op.cc
+++ nearest_neighbours/cc/ops/nearest_neighbours_op.cc
@@ -5,20 +5,22 @@ using namespace tensorflow;
 using namespace tensorflow::shape_inference;
 
 REGISTER_OP("NearestNeighbours")
-        .Input("token_embeddings: float32")
-        .Input("embedding_matrix: float32")
-        .Output("nearest_neighbours: float32")
-        .SetShapeFn([](InferenceContext *c) {
-                      ShapeHandle token_embeddings_shape_handle, embedding_matrix_shape_handle;
-                      TF_RETURN_IF_ERROR(c->WithRank(c->input(0), 3, &token_embeddings_shape_handle));
-                      TF_RETURN_IF_ERROR(c->WithRank(c->input(1), 2, &embedding_matrix_shape_handle));
+    .Input("token_embeddings: float32")
+    .Input("embedding_matrix: float32")
+    .Output("nearest_neighbours: float32")
+    .SetShapeFn([](InferenceContext *c) {
+      ShapeHandle token_embeddings_shape_handle, embedding_matrix_shape_handle;
+      TF_RETURN_IF_ERROR(
+          c->WithRank(c->input(0), 3, &token_embeddings_shape_handle));
+      TF_RETURN_IF_ERROR(
+          c->WithRank(c->input(1), 2, &embedding_matrix_shape_handle));
 
-                      c->set_output(0, c->MakeShape(
-                          {c->Dim(token_embeddings_shape_handle, 0), c->Dim(token_embeddings_shape_handle, 1),
-                           c->Dim(token_embeddings_shape_handle, 2)}));
-                      return OkStatus();
-                    }
-        );
+      c->set_output(0,
+                    c->MakeShape({c->Dim(token_embeddings_shape_handle, 0),
+                                  c->Dim(token_embeddings_shape_handle, 1),
+                                  c->Dim(token_embeddings_shape_handle, 2)}));
+      return OkStatus();
+    });
 
 
 
