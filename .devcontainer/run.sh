0a754528ab8e72093c1a6b53d735feb6192f7e47
artem-sereda <77972398+artem-sereda@users.noreply.github.com>
2023-04-21 17:19:18 +0200

    :construction:

--- .devcontainer/run.sh
+++ .devcontainer/run.sh
@@ -4,4 +4,4 @@ set -e
 
 PROJECT_DIR=$(realpath "$(pwd)"/..)
 docker run --mount type=bind,source="$PROJECT_DIR",target="/tensorflow_nearest_neighbours" \
-  --platform=linux/x86_64 --rm devcontainer "$1" "$2"
+  --platform=linux/x86_64 --rm devcontainer "$@"
