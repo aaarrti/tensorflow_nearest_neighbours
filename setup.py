628657df6d4629acf6dfe593809a935acfe747ca
artem-sereda <77972398+artem-sereda@users.noreply.github.com>
2023-04-21 16:00:46 +0200

    :construction:

--- setup.py
+++ setup.py
@@ -1,12 +1,6 @@
-from setuptools import find_packages
 from setuptools import setup
-from setuptools.dist import Distribution
 from setuptools.command.install import install
-
-__version__ = "0.0.1"
-
-
-project_name = "tensorflow_nearest_neighbours"
+from setuptools.dist import Distribution
 
 
 class InstallPlatlib(install):
@@ -26,29 +20,6 @@ class BinaryDistribution(Distribution):
 
 
 setup(
-    name=project_name,
-    version=__version__,
-    description="Nearest neighbours word embedding computation.",
-    author="Artem Sereda",
-    author_email="artem.sereda@campus.tu-berlin.de",
-    # Contained modules and scripts.
-    packages=find_packages(),
-    install_requires=[
-        "tensorflow; sys_platform != 'darwin'",
-        "tensorflow_macos; sys_platform == 'darwin'",
-    ],
-    # Add in any packaged data.
-    include_package_data=True,
-    zip_safe=False,
     distclass=BinaryDistribution,
     cmdclass={"install": InstallPlatlib},
-    # PyPI package information.
-    classifiers=[
-        "Intended Audience :: Science/Research",
-        "Programming Language :: Python :: 3.9",
-        "Topic :: Scientific/Engineering :: Mathematics",
-        "Topic :: Software Development :: Libraries :: Python Modules",
-    ],
-    keywords="tensorflow custom op machine learning",
-    python_requires=">=3.8",
 )
